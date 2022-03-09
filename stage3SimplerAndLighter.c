57dc26c33a1b0d4259425e629fb2787206552441
juanDjulioS <88696888+juanDjulioS@users.noreply.github.com>
2022-03-09 09:30:05 -0500

    Update stage3SimplerAndLighter.c

--- stage3SimplerAndLighter.c
+++ stage3SimplerAndLighter.c
@@ -1,4 +1,4 @@
-/***********************************************************************
+/**********************************************************************
 *  Program to alternate between 20 10-led sequences with two buttons,
 *  and to change between 10 velocities with two buttons
 *  Micro: Atmega328P
@@ -29,7 +29,7 @@ short nVel = (&velocities)[1] - velocities;
 int seqArray[][10] =
 {
 	{0},
-	{1,2,4,8,16,32,64,128,256,512},						//1
+	{1,2,4,8,16,32,64,128,256,512},					//1
 	{512,256,128,64,32,16,8,4,2,1},
 	{1,2,0x204,0x109,0x92,0x124,0x242,0x81,0x100,0x200},											
 	{0x200,0x100,0x81,0x242,0x124,0x92,0x109,0x204,2,1},
@@ -38,7 +38,7 @@ int seqArray[][10] =
 	{1023,511,255,127,63,31,15,7,3,1},
 	{0,0x200,0x201,0x301,0x303,0x383,0x387,0x3C7,0x3CF,0x3EF},
 	{0x3EF,0x3CF,0x3C7,0x387,0x383,0x303,0x301,0x201,0x200,0},	
-	{0x3FF,0,0x2AA,0,0x249,0,0x222,0,0x210,0},						          //10
+	{0x3FF,0,0x2AA,0,0x249,0,0x222,0,0x210,0},			//10
 	{0,0x210,0,0x222,0,0x249,0,0x2AA,0,0x3FF},	
 	{0x20,0x30,0x70,0x78,0xF8,0xFA,0x1FA,0x1FE,0x3FE,0x3FF},
 	{0x3FF,0x3FE,0x1FA,0xFA,0xF8,0x78,0x70,0x30,0x20},	
@@ -48,7 +48,7 @@ int seqArray[][10] =
 	{0x108,0x84,0x42,0x21,0x21,0x42,0x84,0x108,0x108,0x84},	
 	{15,189,257,380,504,647,728,882,913,944},
 	{30,161,232,330,431,517,529,544,672,713},
-	{13,21,34,55,89,144,233,377,610,987}						               	//20
+	{13,21,34,55,89,144,233,377,610,987}				//20
 };												
 short nSeq = (&seqArray)[1] - seqArray;
 /********************* Functions and subroutines **********************/	
@@ -96,7 +96,7 @@ void setSeq(int vec[]){
 /************************** Main Program ******************************/
 int main(void)
 {
-	//*******************Ports Setting ********************************/
+	//*******************Ports Setting ****************************/
 	// Port D as output
 	// 0xFF = 0b11111111
 	// PORT B as output
@@ -113,10 +113,10 @@ int main(void)
 	{
 		PORTC = 0x00;
 		getSeq();
-		/******************** Execution of Sequences ******************/
+		/******************** Execution of Sequences ************/
 		switch(seqID)
 		{
-			case 0:						   break;
+			case 0:			       break;
 			case 1:{setSeq(seqArray[1]);   break;}
 			case 2:{setSeq(seqArray[2]);   break;}
 			case 3:{setSeq(seqArray[3]);   break;}
