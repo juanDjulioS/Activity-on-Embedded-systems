d5b619eb2846a16c653381983bb17e97f9d5de09
juanDjulioS <88696888+juanDjulioS@users.noreply.github.com>
2022-03-09 11:12:51 -0500

    Update stage3SimplerAndLighter.c

--- stage3SimplerAndLighter.c
+++ stage3SimplerAndLighter.c
@@ -1,4 +1,4 @@
-/**********************************************************************
+/***********************************************************************
 *  Program to alternate between 20 10-led sequences with two buttons,
 *  and to change between 10 velocities with two buttons
 *  Micro: Atmega328P
@@ -22,14 +22,14 @@
 #define velDown (PINC & (1<<PC2))>>PC2
 
 /******************* Global Variables Definition **********************/
-short seqID = 0;														   
-short velID = 0;														   
-int velocities[] = {2000,500,100};
+short seqID;
+short velID;								   
+int velocities[] = {3000,100};
 short nVel = (&velocities)[1] - velocities;
 int seqArray[][10] =
 {
 	{0},
-	{1,2,4,8,16,32,64,128,256,512},					//1
+	{1,2,4,8,16,32,64,128,256,512},						//1
 	{512,256,128,64,32,16,8,4,2,1},
 	{1,2,0x204,0x109,0x92,0x124,0x242,0x81,0x100,0x200},											
 	{0x200,0x100,0x81,0x242,0x124,0x92,0x109,0x204,2,1},
@@ -38,7 +38,7 @@ int seqArray[][10] =
 	{1023,511,255,127,63,31,15,7,3,1},
 	{0,0x200,0x201,0x301,0x303,0x383,0x387,0x3C7,0x3CF,0x3EF},
 	{0x3EF,0x3CF,0x3C7,0x387,0x383,0x303,0x301,0x201,0x200,0},	
-	{0x3FF,0,0x2AA,0,0x249,0,0x222,0,0x210,0},			//10
+	{0x3FF,0,0x2AA,0,0x249,0,0x222,0,0x210,0},						//10
 	{0,0x210,0,0x222,0,0x249,0,0x2AA,0,0x3FF},	
 	{0x20,0x30,0x70,0x78,0xF8,0xFA,0x1FA,0x1FE,0x3FE,0x3FF},
 	{0x3FF,0x3FE,0x1FA,0xFA,0xF8,0x78,0x70,0x30,0x20},	
@@ -48,21 +48,23 @@ int seqArray[][10] =
 	{0x108,0x84,0x42,0x21,0x21,0x42,0x84,0x108,0x108,0x84},	
 	{15,189,257,380,504,647,728,882,913,944},
 	{30,161,232,330,431,517,529,544,672,713},
-	{13,21,34,55,89,144,233,377,610,987}				//20
+	{13,21,34,55,89,144,233,377,610,987}							//20
 };												
 short nSeq = (&seqArray)[1] - seqArray;
 /********************* Functions and subroutines **********************/	
-void getVel(){
-	if (velID < 0) velID = 0;
-	if (velID > nVel-1) velID = nVel-1;
-	if (velUp) {_delay_ms(250); velID++;}
-	if (velDown) {_delay_ms(250); velID--;}
-}
-void getSeq(){
-	if (seqID < 0) seqID = 0;
-	if (seqID > nSeq-1) seqID = nSeq-1;
-	if (seqUp) {_delay_ms(250); seqID++;}
-	if (seqDown) {_delay_ms(250); seqID--;}
+void indexSelector (short *x,short MAX, int UP, int DOWN){
+	/*
+	x   : variable that depicts an index of an array
+	MAX : size of the array
+	UP  : button to move from 0 to MAX-1 in the array
+	DOWN: button to move from MAX-1 to 0 in the array
+	*/
+	// Avoid index to exceed the limits
+	if ((*x) < 0) (*x) = 0;				
+	if ((*x) > MAX-1) (*x) = MAX-1;
+	// Increase or decrease counting according to the button
+	if (UP)   {_delay_ms(25); (*x)++;}
+	if (DOWN) {_delay_ms(25); (*x)--;}
 }
 void delayms(int n){
 	while(n--)
@@ -75,28 +77,27 @@ void setSeq(int vec[]){
 	PORTB = 0x00;
 	for (int i = 0;i<10; i++)
 	{
-		if (seqUp || seqDown)
-		{
-			break;
-		} 
+		if (seqUp || seqDown) break;
 		else
 		{
-			if (velUp || velDown)
+			if (velUp || velDown) 
 			{
-				getVel();
+				indexSelector(&velID,nVel,velUp,velDown);
 			}
 			PORTD = vec[i] & 0xFF;
 			PORTB = vec[i] >> 8;
 			delayms(velocities[velID]);
 		}
-		
 	}
 }
  
 /************************** Main Program ******************************/
 int main(void)
 {
-	//*******************Ports Setting ****************************/
+	//*************** variable initialization**************************/
+	seqID = 0;
+	velID = 0;
+	//*******************Ports Setting ********************************/
 	// Port D as output
 	// 0xFF = 0b11111111
 	// PORT B as output
@@ -112,31 +113,31 @@ int main(void)
 	while (1)
 	{
 		PORTC = 0x00;
-		getSeq();
-		/******************** Execution of Sequences ************/
+		indexSelector(&seqID,nSeq,seqUp,seqDown);
+		/******************** Execution of Sequences ******************/
 		switch(seqID)
 		{
-			case 0:			       break;
-			case 1:{setSeq(seqArray[1]);   break;}
-			case 2:{setSeq(seqArray[2]);   break;}
-			case 3:{setSeq(seqArray[3]);   break;}
-			case 4:{setSeq(seqArray[4]);   break;}
-			case 5:{setSeq(seqArray[5]);   break;}
-			case 6:{setSeq(seqArray[6]);   break;}
-			case 7:{setSeq(seqArray[7]);   break;}
-			case 8:{setSeq(seqArray[8]);   break;}
-			case 9:{setSeq(seqArray[9]);   break;}
-			case 10:{setSeq(seqArray[10]); break;}
-			case 11:{setSeq(seqArray[11]); break;}
-			case 12:{setSeq(seqArray[12]); break;}
-			case 13:{setSeq(seqArray[13]); break;}
-			case 14:{setSeq(seqArray[14]); break;}
-			case 15:{setSeq(seqArray[15]); break;}
-			case 16:{setSeq(seqArray[16]); break;}
-			case 17:{setSeq(seqArray[17]); break;}
-			case 18:{setSeq(seqArray[18]); break;}
-			case 19:{setSeq(seqArray[19]); break;}
-			case 20:{setSeq(seqArray[20]); break;}
+			case 0:		   			      break;
+			case 1:setSeq(seqArray[1]);   break;
+			case 2:setSeq(seqArray[2]);   break;
+			case 3:setSeq(seqArray[3]);   break;
+			case 4:setSeq(seqArray[4]);   break;
+			case 5:setSeq(seqArray[5]);   break;
+			case 6:setSeq(seqArray[6]);   break;
+			case 7:setSeq(seqArray[7]);   break;
+			case 8:setSeq(seqArray[8]);   break;
+			case 9:setSeq(seqArray[9]);   break;
+			case 10:setSeq(seqArray[10]); break;
+			case 11:setSeq(seqArray[11]); break;
+			case 12:setSeq(seqArray[12]); break;
+			case 13:setSeq(seqArray[13]); break;
+			case 14:setSeq(seqArray[14]); break;
+			case 15:setSeq(seqArray[15]); break;
+			case 16:setSeq(seqArray[16]); break;
+			case 17:setSeq(seqArray[17]); break;
+			case 18:setSeq(seqArray[18]); break;
+			case 19:setSeq(seqArray[19]); break;
+			case 20:setSeq(seqArray[20]); break;
 		}
 	}
 	return 0;
