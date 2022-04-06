58dfc89215a6da2cf8c4bdb2d3c9cd4fb1cfa603
juanDjulioS <88696888+juanDjulioS@users.noreply.github.com>
2022-04-06 05:56:02 -0500

    Update lab2Stage1.c

--- lab2Stage1.c
+++ lab2Stage1.c
@@ -8,78 +8,85 @@
 /***************************** Libraries ******************************/
 #include <avr/io.h>
 #include <util/delay.h> //delay library
-
 /******************* Global Variables Definition **********************/
 //common anode (Vcc) representation of each digit
 int dec7seg[10]={0xC0,0xF9,0xA4,0xB0,0x99,0x92,0x82,0xB8,0x80,0x90}; 
-
 // vector to save units, tens, hundreds, and thousands unit of the number
-int digits[] = {0,0,0,0}; 
-int td = 5;			// time in which each display must be on
-int t_long = 100;	// time in milliseconds that a number should last
-
-
+int digits[4] = {0,0,0,0}; 
+int td = 4;			// time in which each display must be on
+int t_long = 16;	// time in milliseconds that a number should last
+int th_mask, hun_mask, ten_mask;
 /********************* Functions and subroutines **********************/
+// function to do delay with variables
 void delayms (int n){
 	while(n--){
 		_delay_ms(1);
 	}
 }
-void numFragmenter(int num){
-	unsigned int i = 0;
-	while(num!=0){
-		digits[i] = num%10;
-		num /=10;
-		i++;
+// function to break up a number into its digits
+void numFragmenter(int num)
+{
+	unsigned int i = 3;
+	while(num != 0)
+	{
+		digits[i]=num % 10;
+		num /= 10;
+		i--;	
 	}
 }
-void display(int thousand, int hundred, int ten, int one){
-	int th_mask = 0b11111110, hun_mask = 0b11111101, te_mask =0b11111011;
-	if (digits[3]== 0 && digits[2] ==  0 && digits[1] ==  0 && (digits[1] <= digits[0])) {
-		th_mask = hun_mask = te_mask = 0xFF;
+// function to avoid displaying zeros in front of a digit whose power 
+// of 10 is less(e.g. 0012 ==> 12)
+void redundant_zeros(int d0, int d1, int d2, int d3){
+	int th_mask = 0x0E, hun_mask = 0x0D, ten_mask =0x0B;
+	if (d0== 0 && (d1 >= d0))
+	{
+		th_mask = 0xFF;
+		if (d1==0 && (d2 >= d1))
+		{
+			hun_mask = 0xFF;
+			if (d2==0 && (d3 >= d2))
+			{
+				ten_mask = 0xFF;
+			}
+		}
 	}
-	if (digits[3]== 0 && digits[2] ==  0 && (digits[2] < digits[1])){
-		th_mask = hun_mask = 0xFF;
-	}
-	if (digits[3]== 0 && (digits[3] < digits[2])) th_mask = 0xFF;
-	
-	PORTB = 0b11111110 | th_mask;		// enable transistor i-th
-	PORTD = thousand;			// show number
+}
+void display(int thousand, int hundred, int ten, int one)
+{	
+	redundant_zeros(digits[0],digits[1],digits[2],digits[3]);
+	PORTB = 0x0E | th_mask;		// enable transistor i-th
+	PORTD = thousand;			    // show number
 	delayms(td);				// delay a time td
-	PORTB = 0b11111101 | hun_mask;
+	PORTB = 0x0D | hun_mask;
 	PORTD = hundred;
 	delayms(td);			
-	PORTB = 0b11111011 | te_mask;		
+	PORTB = 0x0B | ten_mask;		
 	PORTD = ten;
 	delayms(td);		
-	PORTB = 0b11110111;	
+	PORTB = 0x07;	
 	PORTD = one;	
-	delayms(td);		
+	delayms(td);	
 }
-
 /************************** Main Program ******************************/		
 int main(void)
 {
 	/*************** variable initialization **************************/
-	int count = 0;
+	int init = 9999;
+	int count = init;
 	/*******************Ports Setting *********************************/
 	DDRD  = 0xFF; // as output to turn on/off four digit 7 segment displays
 	PORTD = 0xFF; // to enable/disable  PNP transistors 2n3906 (active low)
 	DDRB  = 0x0F;
 	PORTB = 0x0F;
-
-
 	while(1)
 	{
+		if(count < 0) count = init; // restart the counting
+		else for(int i=0; i < 4; i++) digits[i] = 0; // reset digits (clear previous value)
 		numFragmenter(count);
-		for(int i=0; i < t_long/(4*td); i++) 
-		display(dec7seg[digits[3]],dec7seg[digits[2]],dec7seg[digits[1]],dec7seg[digits[0]]);
-		if(count >=9999)
-		{
-			// reset counting and vector of fragmented number returned to zero
-			count = 0;
-			for (int i=0; i<4;i++) digits[i]=0; 
+		for(int i=0; i < t_long/(4*td); i++)
+		{ 
+			display(dec7seg[digits[0]],dec7seg[digits[1]],dec7seg[digits[2]],dec7seg[digits[3]]);
 		}
-		count++;
+		count--;
 	}
 } //** End of Program
