3bcbd4ecd368fee533c7dc28c1b3fba8d49a2bda
juanDjulioS <88696888+juanDjulioS@users.noreply.github.com>
2022-04-09 18:34:20 -0500

    Update lab2Stage3.c

--- lab2Stage3.c
+++ lab2Stage3.c
@@ -1,8 +1,8 @@
 /***********************************************************************
 * a descending counter from 9999 to 0 using a four-digit 7 segment
 * display and buttons to START, STOP and RESET the counting. a PROGRAM 
-* button to change the initial value of the counting (and additionally, 
-* speed up the counter) when is pressed.
+* button to change the initial value of the counting, and additionally, 
+* a button to speed up the counter.
 * Micro: Atmega328P
 * Date: 7-04-2022
 ***********************************************************************/
@@ -11,35 +11,37 @@
 #define START (PINC & (1<<PC5))
 #define RESET (PINC & (1<<PC2))
 #define STOP (PINC & (1<<PC3))
-#define PROGRAM (PINC & (1<<PC4))
+#define SPEED (PINC & (1<<PC4))
+#define PROGRAM (PINC & (1<<PC1))
 /***************************** Libraries ******************************/
 #include <avr/io.h>
 #include <util/delay.h> //delay library
 
 /******************* Global Variables Definition **********************/
 //common anode (Vcc) representation of each digit
-int dec7seg[10]={0xC0,0xF9,0xA4,0xB0,0x99,0x92,0x82,0xF8,0x80,0x90};
+uint8_t dec7seg[10]={0xC0,0xF9,0xA4,0xB0,0x99,0x92,0x82,0xF8,0x80,0x90};
 
 // vector to save units, tens, hundreds, and thousands unit of the number
-int digits[4] = {0,0,0,0}; 
-int td = 5;			// time in which each display must be on
-int t_base = 1000;	// time in milliseconds that a number should last
-int th_mask = 0x0E, hun_mask = 0x0D, ten_mask =0x0B;
-int start_f, stop_f;
-int speed_up_t = 20;
-int init = 9999;
+uint8_t digits[4]; 
+short td = 5;						// time in which each display must be on
+short t_base = 1000;		// time in milliseconds that a number should last
+uint8_t th_mask = 0x0E, hun_mask = 0x0D, ten_mask =0x0B;
+uint8_t start_f, stop_f;   // f_ means "flag"
+uint8_t speed_f;
+uint8_t speed_up_t = 20;
+short init = 9999;
 
 /********************* Functions and subroutines **********************/
 
 // function to do delays with variables
-void delayms (int n){
+void delayms (short n){
 	while(n--){
 		_delay_ms(1);
 	}
 }
 
 // function to break up a number into its digits
-void numFragmenter(int num)
+void numFragmenter(short num)
 {
 	unsigned int i = 3;
 	while(num != 0)
@@ -52,7 +54,7 @@ void numFragmenter(int num)
 
 // function to avoid displaying zeros in front of a digit whose power 
 // of 10 is less(e.g. 0012 ==> 12)
-void redundant_zeros(int d0, int d1, int d2, int d3){
+void redundant_zeros(uint8_t d0, uint8_t d1, uint8_t d2, uint8_t d3){
 	if (d0== 0 && (d1 >= d0))
 	{
 		th_mask = 0xFF;
@@ -67,12 +69,29 @@ void redundant_zeros(int d0, int d1, int d2, int d3){
 	}
 }
 
-void display(int thousand, int hundred, int ten, int one)
+// function to let speed up or not the velocity of the counting
+void debounce_speed_button(uint8_t BUTTON){
+	static unsigned char count = 0;
+	static unsigned char button_state = 0;
+	if (BUTTON != button_state)
+	{
+		count++;
+		if (count >= 4)
+		{
+			button_state = BUTTON;
+			if (BUTTON != 0) speed_f = 1;
+			count = 0;
+		}
+	}
+	else count = 0;
+}
+
+void display(uint8_t thousand, uint8_t hundred, uint8_t ten, uint8_t one)
 {
 	redundant_zeros(digits[0],digits[1],digits[2],digits[3]);
-	PORTB = 0x0E | th_mask;		    // enable transistor i-th
-	PORTD = thousand;				// show number
-	delayms(td);				    // delay a time td
+	PORTB = 0x0E | th_mask;				// enable transistor i-th
+	PORTD = thousand;								// show number
+	delayms(td);								// delay a time td
 	PORTB = 0x0D | hun_mask;
 	PORTD = hundred;
 	delayms(td);			
@@ -89,39 +108,46 @@ void display(int thousand, int hundred, int ten, int one)
 int main(void)
 {
 	/*************** variable initialization **************************/
-
-	int count = init;
-	int new_init = init;
-	int t_long = t_base;
-	int current_count = count;
+	short count = init;
+	short new_init = init;
+	short t_long = t_base;
+	uint8_t on_off_speed = 0;
+	short current_count = count;
 	/*******************Ports Setting *********************************/
 	DDRD |= 0xFF; // as output to turn on/off four digit 7 segment displays
-	PORTD = 0xFF; // to enable/disable  PNP transistors 2n3906 (active low)
-	DDRB  = 0x0F;
+	PORTD = 0xFF; 
+	DDRB  = 0x0F; // to enable/disable  PNP transistors 2n3906 (active low)
 	PORTB = 0x0F;
-	DDRC |= 0x00;  //  all as input
+	DDRC |= 0x00;  //  all as input (buttons with pull-down resistors)
 	
 	while(1)
-	{
+	{	
 		if (START) {start_f = 1; stop_f = 0;}
 		if (start_f == 1)
-		{
-			if(count < 0 || RESET)
-				{
-					if (new_init == 0) count = init; // restart the counting
-					else count = new_init;
-				}
-		
-			else for(int i=0; i < 4; i++) digits[i] = 0; // reset digits (clear previous value)
-			numFragmenter(count);
+		{	
+			if (count < 0 || RESET)
+			{
+				if (new_init == 0) count = init; // restart the counting to the initial value
+				else count = new_init;			// restart to the new initial value
+			}
+			for(int i=0; i < 4; i++) digits[i] = 0; // reset digits (clear previous value)
+			numFragmenter(count);					// break up the number
 			for(int i=0; i < t_long/(4*td); i++)
 			{
 				display(dec7seg[digits[0]],dec7seg[digits[1]],dec7seg[digits[2]],dec7seg[digits[3]]);
+				debounce_speed_button(SPEED);
+				if (speed_f)
+				{
+					speed_f = 0;
+					on_off_speed ^=1;
+				}
+				if(on_off_speed) t_long = speed_up_t;
+				else t_long = t_base;
+				current_count = count; 
+
+				if (PROGRAM && (t_long == t_base)) new_init = current_count;
+				if (STOP) stop_f = 1;
 			}
-			if (STOP) stop_f = 1;
-			if (PROGRAM) {current_count = count; t_long = speed_up_t;}
-			else t_long = t_base;
-			if (t_long == t_base) new_init = current_count;
 			if (!stop_f) count--;
 		}
 	}
