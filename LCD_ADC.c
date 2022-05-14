115b700714cdd2e572b58f2c05526983a58177c1
juanDjulioS <88696888+juanDjulioS@users.noreply.github.com>
2022-05-14 13:55:22 -0500

    Update LCD_ADC.c

--- LCD_ADC.c
+++ LCD_ADC.c
@@ -1,6 +1,3 @@
-/*
- * C code for Digital Voltmeter using ATmega32 Microcontroller
- */ 
 #define F_CPU 16000000UL
 #include <avr/io.h>
 #include <util/delay.h>
@@ -25,6 +22,12 @@ int main(void)
     ADCSRA |=(1<<ADEN)|(1<<ADATE)|(1<<ADPS0)|(1<<ADPS1)|(1<<ADPS2);
 
     char voltageshow [7];
+	int vecMeasu[10];
+	//float vin = 8.82;
+	float vcc = 4.98;
+	double R1 = 1066.0;
+	double R2 = 9930.0;
+	double Req = R1/(R1+R2);	
 
     send_a_command(0x01); //Clear Screen 0x01 = 00000001
     _delay_ms(50);
@@ -32,22 +35,26 @@ int main(void)
     _delay_ms(50);
     send_a_command( 0x0F);
     _delay_ms(50);
-
-    ADCSRA |=(1<<ADSC);
+    ADCSRA |=(1<<ADSC); 
     while(1)
-    {
-        float voltage = ADC; 
-		send_a_string ("CIRCUIT DIGEST  ");
+    {	int sum = 0;
+		for (int i = 0; i < 10; i++)
+		{
+			vecMeasu[i] = ADC;
+			sum += vecMeasu[i];
+		}
+		float avVolt = sum/10.0;
+		float voltage = (vcc*avVolt) /(1023.0*Req); // 0 - 4.98 v
+		send_a_string ("KaCeJu.nota(5.0)");
 		send_a_command(0x80 + 0x40 + 0);
-        send_a_string ("VOLTAGE=");
+        send_a_string ("VOLTAGE =");
         send_a_command(0x80 + 0x40 + 8);
-        dtostrf(voltage, 5, 3, voltageshow);
+        dtostrf(voltage, 5, 1, voltageshow);
         send_a_string(voltageshow);
         send_a_string("V   ");
         send_a_command(0x80 + 0);
     }
 }
-
 
 void send_a_command(unsigned char command)
 {
@@ -59,7 +66,6 @@ void send_a_command(unsigned char command)
     PORTB &= ~1<<enable;
     PORTD = 0;
 }
-
 
 void send_a_character(unsigned char character)
 {
